496f3f0ad376e449f732daa41e19ec24583ef433
nourbenmoulehem <nbenmoulehom@gmail.com>
2024-02-20 09:48:24 +0100

    Adding redux and react native paper provider, Adding configuration of react native navigation configuration

--- App.tsx
+++ App.tsx
@@ -1,117 +1,48 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
+import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+//React Native Paper
+import {PaperProvider} from 'react-native-paper';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+//Navigation
+import {NavigationContainer} from '@react-navigation/native';
+/* NavigationContainer : wrap the whole app in this container and define the routes so we can navigate between screens */
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+/* createNativeStackNavigator : Stack is the type of Navigation we're using, there is other types like Draws */
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+import MainStack from './src/Navigation/MainStack';
+// Redux
+import {RootState, store} from './src/context/store';
+import {Provider, useSelector} from 'react-redux';
+
+// screens
+import SignIn from './src/screens/sign-in/SignIn';
+import SignUp from './src/screens/sign-up/SignUp';
+
+import AppBar from './src/components/AppBar';
+
+// typescript related, specifying the type
+export type RootStackParamList = {
+  Home: undefined;
+  SignUp: undefined;
+  ForgotPassword: undefined;
+  MultiStepForm: undefined;
+  // Profile: { userId: string };
+  // Feed: { sort: 'latest' | 'top' } | undefined;
+};
+
+const stack = createNativeStackNavigator<RootStackParamList>();
+
+const App = () => {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
+    <Provider store={store}>
+      <PaperProvider>
+        <NavigationContainer>
+          <MainStack />
+        </NavigationContainer>
+      </PaperProvider>
+    </Provider>
   );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            This is assistive banking app.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+};
 
 export default App;
