a864b10dd943463dc55f2097a5ba910ed1551eb7
nourbenmoulehem <nbenmoulehom@gmail.com>
2024-02-20 09:47:12 +0100

    temporary componenet

---
+++ src/components/AppBar.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+
+
+const AppBar = () => {
+  return (
+    <View>
+      <Text>AppBar</Text>
+    </View>
+  )
+}
+
+export default AppBar
+
+const styles = StyleSheet.create({})
