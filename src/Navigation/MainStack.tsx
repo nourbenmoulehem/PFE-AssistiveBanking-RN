bfe6f8ff38ec7fb073ec8f4462cefa2e5f06e653
nourbenmoulehem <nbenmoulehom@gmail.com>
2024-03-03 21:55:53 +0100

    Manage Authentication flow and Add protected flows

--- src/Navigation/MainStack.tsx
+++ src/Navigation/MainStack.tsx
@@ -1,7 +1,7 @@
-import { View, Text, Image } from 'react-native'
-import React from 'react'
+import {View, Text, Image} from 'react-native';
+import React from 'react';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import { tokens } from '../assets/palette';
+import {tokens} from '../assets/palette';
 
 // Navigation
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
@@ -10,17 +10,17 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import SignIn from '../screens/sign-in/SignIn';
 import SignUp from '../screens/sign-up/SignUp';
 import ForgotPassword from '../screens/forgot-password/ForgotPassword';
-import MultiStepForm from '../screens/sign-up/MultiStepForm';
 import AccountActivation from '../screens/account-activation/AccountActivation';
+import Home from '../screens/home/Home';
 
 // Redux
-import { useDispatch, useSelector } from 'react-redux';
-import { RootState } from '../context/store';
-import { setMode } from '../context/globalReducer';
-
+import {useDispatch, useSelector} from 'react-redux';
+import {RootState} from '../context/store';
+import {setMode} from '../context/globalReducer';
 
 export type RootStackParamList = {
   Home: undefined;
+  SignIn: undefined;
   SignUp: undefined;
   ForgotPassword: undefined;
   MultiStepForm: undefined;
@@ -32,63 +32,72 @@ export type RootStackParamList = {
 const stack = createNativeStackNavigator<RootStackParamList>();
 const MainStack = () => {
   const dispatch = useDispatch();
-  const {mode} = useSelector((state: RootState) => state.global);
+  const {mode, isLoggedIn} = useSelector((state: RootState) => state.global);
   const toggleMode = () => {
-    dispatch(setMode())
-  }
+    dispatch(setMode());
+  };
   const colors = tokens(mode);
   return (
     <stack.Navigator
-          screenOptions={{
-            headerStyle: {
-              
-              backgroundColor: colors.main.backgroundColor,
-              
-              
-            },
-            headerShadowVisible: false, // to remove shadow from the header
-            headerTitle: () => (
-              <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
-                <Image
-                  source={require('../assets/logo/logo-webank.png')}
-                  style={{ width: 68, height: 48, marginRight: 10 }}
-                />
-                <Text style={{ color: mode == 'dark' ? "white" : "black", fontSize: 20 , fontWeight: 'bold' }}>
-                  WeBank
-                </Text>
-              </View>
-            ),
-            headerRight: () => (
-              <Icon
-                name={ mode == "dark" ? "brightness-3" : "white-balance-sunny"}
-                size={30}
-                color={colors.main.buttonColor}
-                style={{marginRight: 10}}
-                onPress={toggleMode}
-                
-              />
-            ),
-          }}
-          initialRouteName="Home">
-          <stack.Screen name="Home" component={SignIn} />
-          <stack.Screen
-            name="SignUp"
-            component={SignUp}
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: colors.main.backgroundColor,
+        },
+        headerShadowVisible: false, // to remove shadow from the header
+        headerTitle: () => (
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}>
+            <Image
+              source={require('../assets/logo/logo-webank.png')}
+              style={{width: 68, height: 48, marginRight: 10}}
+            />
+            <Text
+              style={{
+                color: mode == 'dark' ? 'white' : 'black',
+                fontSize: 20,
+                fontWeight: 'bold',
+              }}>
+              WeBank
+            </Text>
+          </View>
+        ),
+        headerRight: () => (
+          <Icon
+            name={mode == 'dark' ? 'brightness-3' : 'white-balance-sunny'}
+            size={30}
+            color={colors.main.buttonColor}
+            style={{marginRight: 10}}
+            onPress={toggleMode}
           />
-          {/* <stack.Screen
-            name="MultiStepForm"
-            component={MultiStepForm}
-          /> */}
-          <stack.Screen
-            name="ForgotPassword"
-            component={ForgotPassword}
-          />
-          <stack.Screen
-            name="AccountActivation"
-            component={AccountActivation}
-          />
-        </stack.Navigator>
-  )
-}
+        ),
+      }}
+      initialRouteName="SignIn">
 
-export default MainStack
+{isLoggedIn ? (
+    <>
+      <stack.Screen
+        name="Home"
+        component={Home}
+        options={{
+          headerShown: true,
+          title: 'Home',
+        }}
+      />
+    </>
+  ) : (
+    <>
+      <stack.Screen name="SignIn" component={SignIn} />
+      <stack.Screen name="SignUp" component={SignUp} />
+      <stack.Screen name="ForgotPassword" component={ForgotPassword} />
+      <stack.Screen name="AccountActivation" component={AccountActivation} />
+    </> 
+  )}
+    </stack.Navigator>
+  );
+};
+
+export default MainStack;
