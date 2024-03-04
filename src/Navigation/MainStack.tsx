e3103002cffbeb4785c87495ed2c6047a5795801
nourbenmoulehem <nbenmoulehom@gmail.com>
2024-03-04 18:53:42 +0100

    Add avatar, logout icon to the header when the user is signed in

--- src/Navigation/MainStack.tsx
+++ src/Navigation/MainStack.tsx
@@ -1,7 +1,9 @@
-import {View, Text, Image} from 'react-native';
-import React from 'react';
+import {View, Text, Image, Touchable, TouchableOpacity} from 'react-native';
+import React, {useEffect, useState} from 'react';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import {tokens} from '../assets/palette';
+import {Avatar} from 'react-native-paper';
+import {ActivityIndicator} from 'react-native';
 
 // Navigation
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
@@ -12,11 +14,15 @@ import SignUp from '../screens/sign-up/SignUp';
 import ForgotPassword from '../screens/forgot-password/ForgotPassword';
 import AccountActivation from '../screens/account-activation/AccountActivation';
 import Home from '../screens/home/Home';
+import Transactions from '../screens/transactions/Transactions';
 
 // Redux
 import {useDispatch, useSelector} from 'react-redux';
 import {RootState} from '../context/store';
-import {setMode} from '../context/globalReducer';
+import {setMode, setInitialLogin, setLogout} from '../context/globalReducer';
+
+// storage
+import * as Keychain from 'react-native-keychain';
 
 export type RootStackParamList = {
   Home: undefined;
@@ -25,6 +31,7 @@ export type RootStackParamList = {
   ForgotPassword: undefined;
   MultiStepForm: undefined;
   AccountActivation: undefined;
+  Transactions: undefined;
   // Profile: { userId: string };
   // Feed: { sort: 'latest' | 'top' } | undefined;
 };
@@ -32,11 +39,32 @@ export type RootStackParamList = {
 const stack = createNativeStackNavigator<RootStackParamList>();
 const MainStack = () => {
   const dispatch = useDispatch();
+  const [loading, setLoading] = useState(false);
   const {mode, isLoggedIn} = useSelector((state: RootState) => state.global);
+  console.log('🚀 ~ MainStack ~ isLoggedIn:', isLoggedIn);
   const toggleMode = () => {
     dispatch(setMode());
   };
   const colors = tokens(mode);
+
+  // to check if the user is logged in or not and update the initial state
+  useEffect(() => {
+    const checkKeychain = async () => {
+      const credentials = await Keychain.getGenericPassword({
+        service: 'accessService',
+      });
+      dispatch(setInitialLogin(credentials !== false));
+    };
+
+    checkKeychain();
+  }, [dispatch]);
+
+  const handleLogout = async () => {
+    await Keychain.resetGenericPassword({service: 'accessService'});
+    await Keychain.resetGenericPassword({service: 'refreshService'});
+    dispatch(setLogout());
+  };
+
   return (
     <stack.Navigator
       screenOptions={{
@@ -66,36 +94,74 @@ const MainStack = () => {
           </View>
         ),
         headerRight: () => (
-          <Icon
-            name={mode == 'dark' ? 'brightness-3' : 'white-balance-sunny'}
-            size={30}
-            color={colors.main.buttonColor}
-            style={{marginRight: 10}}
-            onPress={toggleMode}
-          />
+          <>
+            <Icon
+              name={mode == 'dark' ? 'brightness-3' : 'white-balance-sunny'}
+              size={30}
+              color={colors.main.buttonColor}
+              style={{marginRight: 10}}
+              onPress={toggleMode}
+            />
+            {isLoggedIn ? (
+              loading ? (
+                <ActivityIndicator
+                  size="large"
+                  color={colors.main.buttonColor}
+                />
+              ) : (
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    gap: 15,
+                  }}>
+                  <Avatar.Text size={30} label="XD" />
+                  <TouchableOpacity onPress={handleLogout}>
+                    <Icon
+                      name="logout"
+                      size={30}
+                      color={colors.main.buttonColor}
+                      style={{marginRight: 10}}
+                    />
+                  </TouchableOpacity>
+                </View>
+              )
+            ) : null}
+          </>
         ),
       }}
       initialRouteName="SignIn">
-
-{isLoggedIn ? (
-    <>
-      <stack.Screen
-        name="Home"
-        component={Home}
-        options={{
-          headerShown: true,
-          title: 'Home',
-        }}
-      />
-    </>
-  ) : (
-    <>
-      <stack.Screen name="SignIn" component={SignIn} />
-      <stack.Screen name="SignUp" component={SignUp} />
-      <stack.Screen name="ForgotPassword" component={ForgotPassword} />
-      <stack.Screen name="AccountActivation" component={AccountActivation} />
-    </> 
-  )}
+      {isLoggedIn ? (
+        <>
+          <stack.Screen
+            name="Home"
+            component={Home}
+            options={{
+              headerShown: true,
+              title: 'Home',
+            }}
+          />
+          <stack.Screen
+            name="Transactions"
+            component={Transactions}
+            options={{
+              headerShown: true,
+              title: 'Transactions',
+            }}
+          />
+        </>
+      ) : (
+        <>
+          <stack.Screen name="SignIn" component={SignIn} />
+          <stack.Screen name="SignUp" component={SignUp} />
+          <stack.Screen name="ForgotPassword" component={ForgotPassword} />
+          <stack.Screen
+            name="AccountActivation"
+            component={AccountActivation}
+          />
+        </>
+      )}
     </stack.Navigator>
   );
 };
