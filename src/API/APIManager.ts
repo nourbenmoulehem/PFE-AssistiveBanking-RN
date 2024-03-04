640991da1f6993d7436e1bd7c139434a9dad896d
nourbenmoulehem <nbenmoulehom@gmail.com>
2024-03-04 18:50:46 +0100

    Add axios interceptor to verify and refresh when access  token is expired

--- src/API/APIManager.ts
+++ src/API/APIManager.ts
@@ -1,9 +1,76 @@
-import axios from 'axios';
+import axios, {AxiosInstance} from 'axios';
+import * as Keychain from 'react-native-keychain';
+import {jwtDecode} from 'jwt-decode';
+import dayjs from 'dayjs';
+import base64 from 'base-64';
+import { API_BASE_URL } from '@env';
 
-const api = axios.create({
-  baseURL: process.env.API_URL,
-  withCredentials: true,
-  responseType: 'json',
-});
+// import { setLogout } from '../context/globalReducer';
+// import { useDispatch } from 'react-redux';
 
-export default api;
+global.atob = base64.decode; // for jwt-decode
+
+
+
+const getApi = async (): Promise<AxiosInstance | undefined> => {
+  console.log('🚀 ~ getApi ~ getApi');
+
+  
+  try {
+    const accessToken = await Keychain.getGenericPassword({service: 'accessService'});
+    const refreshToken = await Keychain.getGenericPassword({service: 'refreshService'});
+    
+    if (accessToken && refreshToken) {
+
+      const axiosInstance = axios.create({
+        baseURL: process.env.API_BASE_URL,
+        withCredentials: true,
+        headers: {Authorization: `Bearer ${refreshToken.password}`},
+        responseType: 'json',
+      });
+      
+      
+      // on request middleware to verify token
+      axiosInstance.interceptors.request.use(async req => {
+        
+        // check if access token is expired
+        const user = jwtDecode(accessToken.password);
+        console.log('🚀 ~ getApi ~ user:', user);
+        const isExpired = dayjs.unix(user.exp as number).diff(dayjs()) < 1;
+        // isExpired true then token has expired if false then token is still valid
+        console.log('🚀 ~ getApi ~ isExpired:', isExpired);
+        
+        // if token is not expired then return the request and move on to the actual request
+        if (!isExpired) return req;
+
+        // if token is expired then refresh the access token
+        const response = await axios.post(`${API_BASE_URL}/api/v1/auth/refresh-token`, {}, {
+          headers: {Authorization: `Bearer ${refreshToken.password}`},
+        });
+        console.log('🚀 ~ getApi ~ response access_token:', response.data.access_token);
+        console.log('🚀 ~ getApi ~ response refersh:', response.data.refresh_token);
+
+        // save new access token
+        await Keychain.setGenericPassword('accessToken', response.data.access_token, {service: 'accessService'});
+        // i might delete cause no need, but let's keep it for now
+        await Keychain.setGenericPassword('refreshToken', response.data.refresh_token, {service: 'refreshService'});
+
+        return req;
+      });
+      
+      
+
+      return axiosInstance ;
+    } else {
+      console.log('log out!!!');
+
+      // Dispatch logout action
+      // dispatch(logout());
+    }
+  } catch (error) {
+    console.error('Failed to get credentials:', error);
+    // Handle error (e.g., show an error message to the user)
+  }
+};
+
+export default getApi;
