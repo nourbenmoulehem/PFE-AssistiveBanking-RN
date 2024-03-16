be3d5e4e135bd612468bda4574cce32381951cf6
nourbenmoulehem <nbenmoulehom@gmail.com>
2024-03-16 23:41:07 +0100

    Modify Modal (pop-up) to handle message from server's responses

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,20 +1,29 @@
 import React from 'react';
-import { Modal, Portal, Text, Button } from 'react-native-paper';
+import { Modal, Portal, Text, Button, ActivityIndicator } from 'react-native-paper';
 
 interface ErrorModalProps {
   visible: boolean;
-  error: string;
+  error?: string;
+  isLoading?: boolean;
+  isRedirectSignIn?: boolean;
   onClose: () => void;
+
 }
 
-const ErrorModal: React.FC<ErrorModalProps> = ({ visible, error, onClose }) => {
+const ErrorModal: React.FC<ErrorModalProps> = ({ visible, error, isLoading, isRedirectSignIn, onClose }) => {
+  console.log(isRedirectSignIn, 'isRedirectSignIn'); // TODO: when isRedirectSignIn is true, redirect to SignIn screen when clicking on the button "Fermer" and modify accessibility label or hint with the appropriate message
+  
   return (
     <Portal>
       <Modal visible={visible} onDismiss={onClose} contentContainerStyle={styles.modalContainer}>
-        <Text style={styles.errorText}>{error}</Text>
-        <Button mode="contained" onPress={onClose} accessibilityLabel="Fermer">
-          Fermer
-        </Button>
+        { isLoading ? <ActivityIndicator /> : 
+          <>
+            <Text style={styles.errorText}>{error}</Text>
+            <Button mode="contained" onPress={onClose} accessibilityLabel="Fermer">
+              Fermer
+            </Button>
+          </>
+        }
       </Modal>
     </Portal>
   );
