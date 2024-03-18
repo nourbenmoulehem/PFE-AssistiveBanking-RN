a64a53cc3e57dc7449e02c243a48e1331a352ffe
nourbenmoulehem <nbenmoulehom@gmail.com>
2024-03-18 11:49:56 +0100

    Updated button to redirect to sign-in page upon success - implementation pending

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import { Modal, Portal, Text, Button, ActivityIndicator } from 'react-native-paper';
 
+// navigation
+import {RootStackParamList} from '../../App';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+
+
+
 interface ErrorModalProps {
   visible: boolean;
   error?: string;
   isLoading?: boolean;
   isRedirectSignIn?: boolean;
   onClose: () => void;
-
+  navigation?: NativeStackScreenProps<
+    RootStackParamList,
+    'SignIn'
+  >['navigation'];
 }
 
-const ErrorModal: React.FC<ErrorModalProps> = ({ visible, error, isLoading, isRedirectSignIn, onClose }) => {
+const ErrorModal: React.FC<ErrorModalProps> = ({ visible, error, isLoading, isRedirectSignIn, navigation, onClose }) => {
   console.log(isRedirectSignIn, 'isRedirectSignIn'); // TODO: when isRedirectSignIn is true, redirect to SignIn screen when clicking on the button "Fermer" and modify accessibility label or hint with the appropriate message
+
   
   return (
     <Portal>
@@ -19,7 +29,13 @@ const ErrorModal: React.FC<ErrorModalProps> = ({ visible, error, isLoading, isRe
         { isLoading ? <ActivityIndicator /> : 
           <>
             <Text style={styles.errorText}>{error}</Text>
-            <Button mode="contained" onPress={onClose} accessibilityLabel="Fermer">
+            <Button mode="contained" onPress={() => {
+    if (isRedirectSignIn) {
+      navigation?.navigate('SignIn'); // Replace 'SignIn' with your actual SignIn route
+    } else {
+      onClose();
+    }
+  }}  accessibilityLabel="Fermer">
               Fermer
             </Button>
           </>
