411639a8c532cb8210f5a3dee690280257d0c022
nourbenmoulehom <nbenmoulehom@gmail.com>
2024-03-26 22:52:50 +0100

    configure store adding redux query reducer and middleware

--- src/context/store.ts
+++ src/context/store.ts
@@ -1,10 +1,13 @@
 import {configureStore} from '@reduxjs/toolkit';
 import globalReducer from './globalReducer';
-
+import {clientApi} from '../API/ClientApi';
 export const store = configureStore({
   reducer: {
     global: globalReducer, // global is the name of the reducer
+    [clientApi.reducerPath]: clientApi.reducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware().concat(clientApi.middleware),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
