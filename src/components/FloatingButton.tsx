ddadd4e5ad05a9f1227b265a988cebab3dd9184d
nourbenmoulehom <nbenmoulehom@gmail.com>
2024-03-27 15:36:09 +0100

    detect wake word webank with picovoice

--- src/components/FloatingButton.tsx
+++ src/components/FloatingButton.tsx
@@ -6,11 +6,27 @@ import {
   BuiltInKeywords,
   PorcupineErrors,
 } from '@picovoice/porcupine-react-native';
+import RNFS from 'react-native-fs';
+
+// redux
+import {useSelector} from 'react-redux';
+import {RootState} from '../context/store';
+import {tokens} from '../assets/palette';
+
+let floatButtonBackgroundColor: string = '';
 
 const FloatingButton = () => {
+  const [isDetected, setIsDetected] = React.useState(false);
+
+  const {mode} = useSelector((state: RootState) => state.global);
+  const colors = tokens(mode);
   let recordAudioRequest;
   const ACCESS_KEY = "pfkjx/OKWZLZi4tC/3OSSr027rsRC0ToMGypPUMwUleYrqjjHfPmdA==";
-  const KEYWORD_FILE_PATH = "../";
+  // let keywordName = 'hey_siri'; // we-bank_en_android_v3_0_0
+  // let keywordFilename = `${keywordName}.ppn`;
+  const keywordFilename = 'we-bank_en_android_v3_0_0.ppn';
+  let keywordPath;
+  const isAndroid = Platform.OS === 'android';
 
   const _requestRecordAudioPermission = async () => {
     const granted = await PermissionsAndroid.request(
@@ -42,15 +58,48 @@ const FloatingButton = () => {
       console.log('Permission granted');
 
       try {
+        if (isAndroid) {
+          console.log(RNFS.DocumentDirectoryPath);
+  
+          keywordPath = `${RNFS.DocumentDirectoryPath}/${keywordFilename}`;
+  
+          // let files = await RNFS.readDir(RNFS.DocumentDirectoryPath);
+          // console.log("Files in directory:");
+          // files.forEach(file => {
+          //   console.log(file.name);
+          // });;
+  
+          await RNFS.copyFileAssets(keywordFilename, keywordPath);
+          
+        } else {
+          keywordPath = `${RNFS.MainBundlePath}/${keywordFilename}`;
+        }
+
         const detectionCallback = (keyword: any) => {
+          console.log('Detection callback called');
+
+          // setIsDetected(true);
+          
           console.log(`Keyword detected: ${keyword}`);
         };
   
+        // let porcupineManager = await PorcupineManager.fromBuiltInKeywords(
+        //   ACCESS_KEY,
+        //   [BuiltInKeywords.BUMBLEBEE],
+        //   detectionCallback
+        // );
+        
         let porcupineManager = await PorcupineManager.fromKeywordPaths(
           ACCESS_KEY,
-          ["../android/app/src/main/assets/we-bank_en_android_v3_0_0.ppn"],
+          [keywordPath],
           detectionCallback
         );
+
+        await porcupineManager.start();
+        console.log('PorcupineManager started');
+        console.log("🚀 ~ recordAudioRequest.then ~ porcupineManager:", porcupineManager)
+        
+
       } catch (error) {
         console.log('Error initializing Porcupine:', error);
         
@@ -61,6 +110,15 @@ const FloatingButton = () => {
   });
 
   
+  const styles = StyleSheet.create({
+    fab: {
+      position: 'absolute',
+      margin: 16,
+      right: 0,
+      bottom: 0,
+      backgroundColor: colors.accent[400],
+    },
+  });
   
   return (
     <FAB
@@ -71,13 +129,6 @@ const FloatingButton = () => {
   );
 };
 
-const styles = StyleSheet.create({
-  fab: {
-    position: 'absolute',
-    margin: 16,
-    right: 0,
-    bottom: 0,
-  },
-});
-
 export default FloatingButton;
+
+
